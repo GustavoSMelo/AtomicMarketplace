01940848feef5f6f0d273594882f2866b080d868
Gustavo Santos Melo <gsantos15569@gmail.com>
2020-08-20 19:48:35 -0300

    working in main page

--- web/src/views/main/index.tsx
+++ web/src/views/main/index.tsx
@@ -4,13 +4,47 @@ import './style.css'
 import { FaRegHeart, FaCartPlus, FaStream, FaStar } from 'react-icons/fa'
 import { Link } from 'react-router-dom'
 
+import Clock from '../../assets/images/icons/relogio.svg'
+import Tshirt from '../../assets/images/icons/camiseta.svg'
+import Shoe from '../../assets/images/icons/sapato.svg'
+import Sneakers from '../../assets/images/icons/tenis.svg'
+import Social from '../../assets/images/icons/gravata.svg'
+
 const Main = () => {
   return (
     <>
       <Header />
       <main>
+        <section className='menu'>
+          <aside>
+            <ul>
+              <li className='list-card'>
+                <img src={Clock} />  Novos produtos
+              </li>
+
+              <li className='list-card'>
+                <img src={Shoe} /> Sapatos
+              </li>
+
+              <li className='list-card'>
+                <img src={Sneakers} /> Ténis
+              </li>
+
+              <li className='list-card'>
+                <img src={Tshirt} /> Camisetas
+              </li>
+
+              <li className='list-card'>
+                <img src={Social} /> Social
+              </li>
+            </ul>
+          </aside>
+          <article>
+
+          </article>
+        </section>
         <ul data-testid='product-list'>
-          <li data-testid='product'>
+          <li data-testid='product' className='list-card'>
             <span className='IconContainer'>
               <span className='heart'>
                 <FaRegHeart color={'#f00'} />
