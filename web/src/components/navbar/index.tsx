a609163a473896ec4c78bb56e8b498c1d512dd46
Gustavo Santos Melo <gsantos15569@gmail.com>
2020-08-25 22:33:55 -0300

    working in navbar when user do login

--- web/src/components/navbar/index.tsx
+++ web/src/components/navbar/index.tsx
@@ -1,38 +1,86 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
-
-import { FaSearch } from 'react-icons/fa'
+import { FaSearch, FaRegUserCircle, FaShoppingBag } from 'react-icons/fa'
 import './style.css'
+import isLogged from '../../utils/VerifyLogin'
 
 const Navbar = () => {
-  return (
-    <header>
-      <figure>
-        <h1>logo</h1>
-      </figure>
-      <nav>
-        <Link className='Link' to='/'>
-          <h2>
-            Camisas
-          </h2>
-        </Link>
-        <Link className='Link' to='/'>
-          <h2>
-            Sapatos
-          </h2>
-        </Link>
-      </nav>
+  const [log, setLog] = useState(false)
 
-      <section>
-        <Link className='search' to='/'>
-          <FaSearch />
-        </Link>
-        <Link className='Button' to='/'>
-          <h2>Login</h2>
-        </Link>
-      </section>
-    </header>
-  )
+  const userHaveLogin = async () => {
+    const response = await isLogged()
+    setLog(response)
+  }
+  useEffect(() => { userHaveLogin() }, [log])
+
+  const Layout = () => {
+    if (log) {
+      return (
+        <>
+          <header>
+            <figure>
+              <h1>logo</h1>
+            </figure>
+            <nav>
+              <Link className='Link' to='/'>
+                <h2>
+                  Camisas
+                </h2>
+              </Link>
+              <Link className='Link' to='/'>
+                <h2>
+                  Sapatos
+                </h2>
+              </Link>
+            </nav>
+
+            <section>
+              <Link className='Touchable' to='/'>
+                <FaSearch />
+              </Link>
+              <Link className='Touchable Bag' to='/'>
+                <FaShoppingBag />
+              </Link>
+              <Link className='Touchable' to='/'>
+                <FaRegUserCircle />
+              </Link>
+            </section>
+          </header>
+        </>
+      )
+    } else {
+      return (
+        <header>
+          <figure>
+            <h1>logo</h1>
+          </figure>
+          <nav>
+            <Link className='Link' to='/'>
+              <h2>
+                Camisas
+              </h2>
+            </Link>
+            <Link className='Link' to='/'>
+              <h2>
+                Sapatos
+              </h2>
+            </Link>
+          </nav>
+
+          <section>
+            <Link className='Touchable' to='/'>
+              <FaSearch />
+            </Link>
+            <Link className='Button' to='/'>
+              <h2>Login</h2>
+            </Link>
+          </section>
+        </header>
+      )
+    }
+  }
+
+  return Layout()
 }
 
 export default Navbar
