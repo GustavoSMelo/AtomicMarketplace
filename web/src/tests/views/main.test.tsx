a94035017bb7571e47a646cfcc9845c6e6e944db
Gustavo Santos Melo <gsantos15569@gmail.com>
2020-08-26 23:58:50 -0300

    resolving error

--- web/src/tests/views/main.test.tsx
+++ web/src/tests/views/main.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { render, } from '@testing-library/react'
+import { render } from '@testing-library/react'
 import Main from '../../views/main'
 import { BrowserRouter } from 'react-router-dom'
 
