34727c5c4eec0d606e24d14b5bf74ebdd0c85ade
Gustavo Santos Melo <gsantos15569@gmail.com>
2020-08-27 00:36:50 -0300

    footer done (need to do responsive mode yet)

--- web/src/components/footer/styled.ts
+++ web/src/components/footer/styled.ts
@@ -7,14 +7,14 @@ export const Container = styled.footer`
   flex-direction: row;
   padding: 30px;
 
-  section{
+  aside{
     display: grid;
     grid-template-columns: repeat(2, 1fr);
     padding: 40px;
-    border-right: 2px solid #909090
+    border-right: 2px solid #909090;
   }
 
-  section > span{
+  aside > span{
     display: flex;
     flex-direction: column;
     margin: 15px;
@@ -24,11 +24,26 @@ export const Container = styled.footer`
 
   article{
     padding: 30px;
+    border-right: 2px solid #909090;
   }
 
   article > ul > li{
     padding: 5px;
     list-style-type: none;
     cursor: pointer;
+  }
+
+  section{
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    padding: 40px;
+  }
+
+  section > span{
+    display: flex;
+    flex-direction: column;
+    margin: 15px;
+    align-items: center;
+    justify-content: center;
   }
 `
