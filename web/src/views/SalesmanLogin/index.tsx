8296d290b044a7a9585b5cb1ff31864c53044b2b
mateusamarall <mateus.amaral018@gmail.com>
2020-08-27 23:23:25 -0300

    salesman login and signup page created

---
+++ web/src/views/SalesmanLogin/index.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+
+import Navbar from '../../components/navbar'
+import Button from '../../components/button'
+import Input from '../../components/input'
+import logo from '../../assets/images/salesman-login-image.png'
+import { FaSignInAlt } from 'react-icons/fa'
+import './style.css'
+import { Link } from 'react-router-dom'
+
+const SalesmanLogin: React.FC = () => {
+  return (
+    <>
+      <Navbar />
+      <div className="container-salesman-login">
+        <section className="form">
+          <img src={logo} alt="image login" />
+          <form data-testid="form">
+            <h1>
+              Bem vindo,
+              <br />
+              <span>Faça seu login vendedor</span>
+            </h1>
+            <Input data-testid="login-input" type="text" placeholder="Seu CPNJ" />
+            <Input data-testid="password-input" type="password" placeholder="Sua Senha" />
+
+            <Button type="button" value="Entrar" /> <br /> <br />
+            <Link data-testid="registry-button" className="link-register" to="/salesman/signup">
+              <FaSignInAlt fontSize={15} /> Realizar Cadastro
+            </Link>
+          </form>
+        </section>
+      </div>
+    </>
+  )
+}
+
+export default SalesmanLogin
---
+++ web/src/views/SalesmanSignup/index.tsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import Navbar from '../../components/navbar'
+import Button from '../../components/button'
+import Input from '../../components/input'
+import { Link } from 'react-router-dom'
+import logo from '../../assets/images/salesman-signup-image.png'
+import { FaArrowLeft } from 'react-icons/fa'
+import './style.css'
+
+const SalesmanSignup: React.FC = () => {
+  return (
+    <>
+      <Navbar />
+      <div className="container-signup">
+        <section className="form">
+          <Link to="/salesman/login" className="arrow-back" >
+            <FaArrowLeft fontSize={20} />
+          </Link>
+          <img src={logo} alt="image signup" />
+          <form>
+            <h1>
+              Bem vindo,
+              <br />
+              <span>Se registre-se e comece vender seus produtos</span>
+            </h1>
+            <Input type="text" placeholder="Nome da Empresa" />
+            <Input type="text" placeholder="Seu CPNJ" />
+            <Input type="text" placeholder="Seu Endereço" />
+            <Input type="email" placeholder="Seu E-mail para contato" />
+            <Input type="password" placeholder="Sua Senha" />
+            <br />
+            <Button type="button" value="Registrar" />
+
+          </form>
+        </section>
+      </div>
+    </>
+  )
+}
+
+export default SalesmanSignup
