9858f5eb25d3f15b7c6a22a66da9e9e0d07b553e
mateusamarall <mateus.amaral018@gmail.com>
2020-09-01 17:52:13 -0300

    edited input for min value

--- web/src/components/input/index.tsx
+++ web/src/components/input/index.tsx
@@ -4,11 +4,12 @@ import './style.css'
 interface IntputProps {
   placeholder: string;
   type: string;
+  min?: string;
 }
-const input: React.FC<IntputProps> = ({ placeholder, type }) => {
+const input: React.FC<IntputProps> = ({ placeholder, type, min }) => {
   return (
     <>
-      <input data-testid='input-component' className="input-data" type={type} placeholder={placeholder} required />
+      <input data-testid='input-component' className="input-data" type={type} placeholder={placeholder} min={min} required />
     </>
   )
 }
