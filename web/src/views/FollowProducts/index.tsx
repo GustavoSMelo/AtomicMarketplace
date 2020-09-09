bd288d53a06dc3156be4eb92e5bb1d640be6a7b0
Gustavo Santos Melo <gsantos15569@gmail.com>
2020-09-08 22:38:08 -0300

    Creating view of products already buyed

---
+++ web/src/views/FollowProducts/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Navbar from '../../components/navbar'
+import { Container } from './styled'
+
+const FollowProducts = () => {
+  return (
+    <>
+      <Navbar />
+      <Container>
+        <h1>Esses sao os pedidos a caminho: </h1>
+
+        <article className='Card'>
+
+        </article>
+      </Container>
+    </>
+  )
+}
+
+export default FollowProducts
---
+++ web/src/views/FollowProducts/styled.ts
@@ -0,0 +1,20 @@
+import styled from 'styled-components'
+
+export const Container = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  h1{
+    margin-top: 80px;
+    text-align: center;
+    margin-bottom: 20px;
+  }
+  .Card{
+    padding: 15px;
+    border: solid 2px #000;
+    width: 60%;
+    border-radius: 5px;
+  }
+`
