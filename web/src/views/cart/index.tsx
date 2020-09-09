1de6c8044392861b0597601345060c4da35da895
Gustavo Santos Melo <gsantos15569@gmail.com>
2020-09-08 22:40:45 -0300

    page of cart done

--- web/src/views/cart/index.tsx
+++ web/src/views/cart/index.tsx
@@ -4,23 +4,27 @@ import Navbar from '../../components/navbar'
 import Footer from '../../components/footer'
 import EmptyShoppingCart from '../../assets/images/empty-shopping-cart.png'
 import { RiShip2Line } from 'react-icons/ri'
-import { FaRegTrashAlt } from 'react-icons/fa'
+import { FaRegTrashAlt, FaArrowRight } from 'react-icons/fa'
 import ImageTest from '../../assets/images/bannershoes2.png'
+import { Link } from 'react-router-dom'
 
 const Cart = () => {
   return (
     <>
       <Navbar />
-      <Container data-testid='Container' haveProduct={true}>
+      <Container data-testid='Container' haveProduct={false}>
         <section data-testid='without-product'>
           <img data-testid='image-alert' src={EmptyShoppingCart} alt='Empty shopping cart' />
           <h1>Você não tem produtos na sacola ainda ;-;</h1>
           <h2>Navegue na nossa loja para encontrar produtos</h2>
           <button data-testid='btn-navigate' type='button'>
-            <RiShip2Line color={'#fff'} fontSize={26} /> Navegar
+            <Link className='link' to='/search'>
+              <RiShip2Line color={'#fff'} fontSize={26} /> Navegar
+            </Link>
           </button>
         </section>
         <article data-testid='with-product'>
+          <h1>Valor total da sacola: 120.00 BRL</h1>
           <ul data-testid='list'>
             <li>
               <figure>
@@ -37,6 +41,7 @@ const Cart = () => {
               </span>
             </li>
           </ul>
+          <button type="button">Continuar compra <FaArrowRight size={16} style={{ margin: 10 }} /></button>
         </article>
       </Container>
       <Footer />
