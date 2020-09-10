1e21c1483189b566ac612c3445aefd7688314246
Gustavo Santos Melo <gsantos15569@gmail.com>
2020-09-09 23:43:02 -0300

    styled of page to see products already buyed

--- web/src/views/FollowProducts/styled.ts
+++ web/src/views/FollowProducts/styled.ts
@@ -6,6 +6,11 @@ export const Container = styled.main`
   align-items: center;
   justify-content: center;
 
+  .Icon{
+    margin-top: 120px;
+    cursor: pointer;
+  }
+
   h1{
     margin-top: 80px;
     text-align: center;
@@ -16,5 +21,52 @@ export const Container = styled.main`
     border: solid 2px #000;
     width: 60%;
     border-radius: 5px;
+    display: flex;
+    flex-direction: row;
   }
+  .Card > figure{
+    width: 40%;
+  }
+
+  .Card > figure > img{
+    width: 300px;
+  }
+
+  .Card > section {
+    display: flex;
+    flex-direction: column;
+    width: 60%;
+    align-items: baseline;
+    padding: 10px;
+  }
+
+  .Card > section > h1{
+    font-size: 24pt;
+    text-align: left;
+  }
+
+  .Card > section > h2{
+    margin-bottom: 50px;
+  }
+
+  .Card > section > span{
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+  }
+
+  .Card > section > span > button{
+    border: none;
+    padding: 30px;
+    border-radius: 5px;
+    cursor: pointer;
+    font-size: 16pt;
+  }
+
+  .Card > section > span > article{
+    font-size: 18pt;
+  }
+
 `
