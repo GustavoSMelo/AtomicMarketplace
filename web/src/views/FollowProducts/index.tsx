1ae0abeea7d5edc76f3219cf8d144ddcc66f0f9d
Gustavo Santos Melo <gsantos15569@gmail.com>
2020-09-09 23:42:06 -0300

    Page to follow products already buyed

--- web/src/views/FollowProducts/index.tsx
+++ web/src/views/FollowProducts/index.tsx
@@ -1,16 +1,32 @@
 import React from 'react'
 import Navbar from '../../components/navbar'
 import { Container } from './styled'
+import Image from '../../assets/images/bannershoes2.png'
+import { FaTruckLoading, FaArrowLeft } from 'react-icons/fa'
 
 const FollowProducts = () => {
   return (
     <>
       <Navbar />
       <Container>
-        <h1>Esses sao os pedidos a caminho: </h1>
+        <FaArrowLeft className="Icon" size={32} />
+
+        <h1> Esses sao os pedidos a caminho: </h1>
 
         <article className='Card'>
-
+          <figure>
+            <img src={Image} alt="product" />
+          </figure>
+          <section>
+            <h1>Name of product</h1>
+            <h2>120.00 BRL</h2>
+            <span>
+              <button type="button">Visualizar entrega</button>
+              <article>
+                <FaTruckLoading /> Status da entrega
+              </article>
+            </span>
+          </section>
         </article>
       </Container>
     </>
