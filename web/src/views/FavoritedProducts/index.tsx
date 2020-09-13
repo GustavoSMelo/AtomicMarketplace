1202f4c64d3748438f024a5863a583bb28a0429c
Gustavo S. Melo <gsantos15569@gmail.com>
2020-09-13 01:26:31 -0300

    working in this view yet

---
+++ web/src/views/FavoritedProducts/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from '../../components/navbar'
+
+const FavoritedProducts = () => {
+  return (
+    <>
+      <Navbar />
+    </>
+  )
+}
+
+export default FavoritedProducts
