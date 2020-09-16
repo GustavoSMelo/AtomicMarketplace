e660e5bc18c24fcba2e96b6d25b2378e4fbadd15
mateusamarall <mateus.amaral018@gmail.com>
2020-09-15 21:54:30 -0300

    mudando a estilização e adicionado interface para o componente

--- web/src/components/ListProduct/index.tsx
+++ web/src/components/ListProduct/index.tsx
@@ -2,23 +2,28 @@ import React from 'react';
 import { RiDeleteBinLine } from 'react-icons/ri'
 import { Main } from './styled';
 
-const ListProduct: React.FC = () => {
+interface listProductProps {
+  image: string;
+  nameProduct: string;
+  date: string;
+  quantity: number;
+}
+const ListProduct: React.FC<listProductProps> = ({ image, nameProduct, date, quantity }) => {
   return <>
     <Main>
       <section>
 
-        <div className="image-product">
-          imagem do produto
-            </div>
+        <img src={image} alt="imagem do produto" />
 
         <div className="data-product">
-          <p>Nome do Produto: <strong>Air Jordan Nike</strong></p>
-          <p className="date-product">Adicionado em: <strong>14/11/2020</strong></p>
-          <p>Quantidade: <strong>50</strong></p>
+          <p>Nome do Produto: <strong>{nameProduct}</strong></p>
+          <p className="date-product">Adicionado em: <strong>{date}</strong></p>
+          <p>Quantidade: <strong>{quantity}</strong></p>
         </div>
 
 
         <div className="buttons-delete">
+          <button>Editar Produto <RiDeleteBinLine color={"yellow"} /></button>
           <button>Deletar Todos <RiDeleteBinLine color={"red"} /></button>
           <button>Deletar um <RiDeleteBinLine color={"red"} /></button>
         </div>
