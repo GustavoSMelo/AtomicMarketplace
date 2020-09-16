7734cfad4147dee4c697d7e02036d96ce0607eff
Gustavo S. Melo <gsantos15569@gmail.com>
2020-09-15 21:01:38 -0300

    models of project

---
+++ backend/app/Models/User.ts
@@ -0,0 +1,31 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+
+export default class User extends BaseModel {
+  @column({ isPrimary: true })
+  public id: number
+
+  @column()
+  public name : string
+
+  @column()
+  public email : string
+
+  @column()
+  public address : string
+
+  @column()
+  public address_number : string
+
+  @column()
+  public cep : string
+
+  @column()
+  public password : string
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+}
