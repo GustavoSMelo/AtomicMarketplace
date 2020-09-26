864a4746b33b741eb051dbad4e07910271a96ff5
Gustavo S. Melo <gsantos15569@gmail.com>
2020-09-26 01:02:54 -0300

    Controller of discounts not done yet

---
+++ backend/app/Controllers/Http/DiscountsController.ts
@@ -0,0 +1,7 @@
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+export default class DiscountsController {
+  public async Store ({request, response} : HttpContextContract) {
+
+  }
+}
