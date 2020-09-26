17bf5c28c7b49d37fc4c6683114ec8c28ffb5bd0
Gustavo S. Melo <gsantos15569@gmail.com>
2020-09-26 00:51:03 -0300

    all routes of application done

--- backend/start/routes.ts
+++ backend/start/routes.ts
@@ -1,29 +1,13 @@
-/*
-|--------------------------------------------------------------------------
-| Routes
-|--------------------------------------------------------------------------
-|
-| This file is dedicated for defining HTTP routes. A single file is enough
-| for majority of projects, however you can define routes in different
-| files and just make sure to import them inside this file. For example
-|
-| Define routes in following two files
-| ├── start/routes/cart.ts
-| ├── start/routes/customer.ts
-|
-| and then import them inside `start/routes/index.ts` as follows
-|
-| import './cart'
-| import './customer'
-|
-*/
-
 import Route from '@ioc:Adonis/Core/Route'
 
 import UserController from '../app/Controllers/Http/UsersController'
 import SessionController from '../app/Controllers/Http/SessionController'
 import ProductsController from '../app/Controllers/Http/ProductsController'
 import SalesmanController from '../app/Controllers/Http/SalesmenController'
+import FavoritesController from '../app/Controllers/Http/FavoritesController'
+import CommentaryController from '../app/Controllers/Http/CommentariesController'
+import VendaController from '../app/Controllers/Http/VendasController'
+import SessionSalesmenController from '../app/Controllers/Http/SessionSalesmenController'
 
 Route.get('/', async () => {
   return { hello: 'world' }
@@ -38,9 +22,31 @@ Route.post('/user/show', new UserController().Show).middleware('deleteShowUser')
 
 //Session User
 Route.post('/user/session', new SessionController().Store).middleware('UserSession')
+Route.post('/salesman/session', new SessionSalesmenController().Store).middleware('UserSession')
 
-//Salesman
+//Salesman - Checked
 Route.post('/salesman', new SalesmanController().Store)
+Route.get('/salesman', new SalesmanController().Index)
+Route.put('/salesman', new SalesmanController().Update).middleware('Auth')
+Route.get('/salesman/show', new SalesmanController().Show)
 
-//Products
-Route.post('/products', new ProductsController().Store)
+//Products - checked
+Route.post('/products', new ProductsController().Store).middleware('Auth')
+Route.get('/products', new ProductsController().Index)
+Route.post('/products/show', new ProductsController().Index)
+Route.put('/products', new ProductsController().Update).middleware('Auth')
+Route.delete('/products', new ProductsController().Destroy)
+
+// Favorites - checked
+Route.post('/favorites', new FavoritesController().Store).middleware('Auth')
+Route.get('/favorites', new FavoritesController().Index).middleware('Auth')
+Route.delete('/favorites', new FavoritesController().Destroy).middleware('Auth')
+
+// Venda - checked
+Route.post('/venda', new VendaController().Store).middleware('Auth')
+Route.get('/venda', new VendaController().Index).middleware('Auth')
+
+// commentary -
+Route.post('/commentary', new CommentaryController().Store).middleware('Auth')
+Route.put('/commentary', new CommentaryController().Update).middleware('Auth')
+Route.get('/commentary', new CommentaryController().Show)
