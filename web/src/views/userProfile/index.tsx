713686b9bda88b737984461938c70c681be88f18
Gustavo S. Melo <gsantos15569@gmail.com>
2020-09-29 17:56:24 -0300

    logic to get data by api done

--- web/src/views/userProfile/index.tsx
+++ web/src/views/userProfile/index.tsx
@@ -1,10 +1,47 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from '../../components/navbar'
 import { Container } from './styled'
 import { FaWallet, FaRegUserCircle, FaPencilAlt, FaTruckLoading, FaHeart, FaClock, FaPowerOff} from 'react-icons/fa'
 import { Link } from 'react-router-dom'
+import api from '../../api'
+import { useHistory } from 'react-router-dom'
 
 const UserProfile = () => {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [state, setState] = useState('')
+  const [neighborhood, setNeighborhood] = useState('')
+  const [cellphone, setCellphone] = useState('')
+  const [cep, setCep] = useState('')
+  const [address, setAddress] = useState('')
+  const [address_number, setAddress_number] = useState('')
+  const history = useHistory()
+
+  const getUserDataByApi = async () => {
+    const response = await api.post('/user/show', { searchid: localStorage.getItem('iduser') })
+    console.log(response.data)
+    setName(response.data.name)
+    setEmail(response.data.email)
+    setState(response.data.state)
+    setNeighborhood(response.data.neighborhood)
+    setCellphone(response.data.cellphone)
+    setCep(response.data.cep)
+    setAddress(response.data.address)
+    setAddress_number(response.data.address_number)
+  }
+
+  const handlerLogoutButtonClick = () => {
+    localStorage.removeItem('token')
+    localStorage.removeItem('iduser')
+    localStorage.removeItem('emailuser')
+    localStorage.removeItem('nameuser')
+    localStorage.setItem('isLogged', 'false')
+  }
+
+  useEffect(() => {
+    getUserDataByApi()
+  }, [])
+
   return (
     <>
       <Navbar />
@@ -14,7 +51,7 @@ const UserProfile = () => {
             <Link className='Link' to='/follow/product'><li> <FaTruckLoading /> Acompanhe seus pedidos</li></Link>
             <Link className='Link' to='/buy/historic'><li> <FaClock /> Historico de pedidos</li></Link>
             <Link className='Link' to='/favorites'><li> <FaHeart /> Produtos favoritados</li></Link>
-            <Link className='Link' to='/'><li> <FaPowerOff /> Logout(Sair) </li></Link>
+            <Link className='Link' onClick={() => handlerLogoutButtonClick()} to='/'><li> <FaPowerOff /> Logout(Sair) </li></Link>
           </ul>
         </aside>
         <article>
@@ -36,27 +73,35 @@ const UserProfile = () => {
           <h1>Dados sobre Name of user</h1>
           <div>
             <span>
-              <h2>Nome completo: </h2> <h3>Gustavo Santos Melo</h3>
+              <h2>Nome completo: </h2> <h3>{name}</h3>
             </span>
 
             <span>
-              <h2>Email: </h2> <h3>gustavo@mail.com</h3>
+              <h2>Email: </h2> <h3>{email}</h3>
             </span>
 
             <span>
-              <h2>Endereço:  </h2> <h3>Rua das palmeiras</h3>
+              <h2>Endereço:  </h2> <h3>{address}</h3>
             </span>
 
             <span>
-              <h2>Numero: </h2> <h3>999</h3>
+              <h2>Numero: </h2> <h3>{address_number}</h3>
             </span>
 
             <span>
-              <h2>CEP: </h2> <h3>999999-999</h3>
+              <h2>CEP: </h2> <h3>{cep}</h3>
             </span>
 
             <span>
-              <h2>Senha: </h2> <h3>***********</h3>
+              <h2>celular: </h2> <h3>{cellphone}</h3>
+            </span>
+
+            <span>
+              <h2>estado: </h2> <h3>{state}</h3>
+            </span>
+
+            <span>
+              <h2>bairro: </h2> <h3>{neighborhood}</h3>
             </span>
 
             <button type="button"><FaPencilAlt /> Editar</button>
