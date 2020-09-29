2ad9e028116f14583cba1d45f002bad4e6afd92e
Gustavo S. Melo <gsantos15569@gmail.com>
2020-09-29 17:42:27 -0300

    System of signup salesman done

--- web/src/views/SalesmanSignup/index.tsx
+++ web/src/views/SalesmanSignup/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from '../../components/navbar'
 import Button from '../../components/button'
 import Input from '../../components/input'
@@ -6,8 +6,33 @@ import { Link } from 'react-router-dom'
 import logo from '../../assets/images/salesman-signup-image.png'
 import { FaArrowLeft } from 'react-icons/fa'
 import './style.css'
+import { useHistory } from 'react-router-dom'
+import PopupCard from '../../components/popupStatusCard'
+import api from '../../api'
 
 const SalesmanSignup: React.FC = () => {
+  const [name, setName] = useState('')
+  const [cnpj, setCnpj] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [address, setAddress] = useState('')
+  const [status, setStatus] = useState(<></>)
+
+  useEffect(() => {
+    setTimeout(() => {
+      setStatus(<></>)
+    }, 3000)
+  }, [status])
+
+  const makeRegistre = async () => {
+    try {
+      await api.post('/salesman', { company_name: name, cnpj, email, password, address })
+      setStatus(<PopupCard backgroundcolor='#51B556' textcolor='#295C2C' content='Vendedor criado com sucesso'/>)
+    } catch (err) {
+      setStatus(<PopupCard backgroundcolor='#FA6450' textcolor='#5C241D' content='Falha ao criar vendedor, verifique os campos e tente novamente'/>)
+    }
+  }
+
   return (
     <>
       <Navbar />
@@ -23,16 +48,18 @@ const SalesmanSignup: React.FC = () => {
               <br />
               <span>Se registre-se e comece vender seus produtos</span>
             </h1>
-            <Input type="text" placeholder="Nome da Empresa" />
-            <Input type="text" placeholder="Seu CPNJ" />
-            <Input type="email" placeholder="Seu E-mail para contato" />
-            <Input type="password" placeholder="Sua Senha" />
+            <Input value={name} onTextChanged={setName} type="text" placeholder="Nome da Empresa" />
+            <Input value={cnpj} onTextChanged={setCnpj} type="text" placeholder="Seu CPNJ" />
+            <Input value={email} onTextChanged={setEmail} type="email" placeholder="Seu E-mail para contato" />
+            <Input value={address} onTextChanged={setAddress} type='text' placeholder='Endereço da sua empresa'/>
+            <Input value={password} onTextChanged={setPassword} type="password" placeholder="Sua Senha" />
             <br />
-            <Link to="/salesman/signupaddress"> <Button type="button" value="Continuar" /></Link>
+            <Button type="button" value="Registrar" onClickFunction={() => makeRegistre()}/>
 
           </form>
         </section>
       </div>
+      {status}
     </>
   )
 }
