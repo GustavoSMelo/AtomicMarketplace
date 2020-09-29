281cc48b8c312a3b9f4779792f042ce5c49f0e07
Gustavo S. Melo <gsantos15569@gmail.com>
2020-09-29 18:01:23 -0300

    hub of actions that salesman can do

---
+++ web/src/views/SalesmanHub/index.tsx
@@ -0,0 +1,95 @@
+import React, { useState, useEffect } from 'react'
+import api from '../../api'
+import { Container } from './styled'
+import EditProfileIcon from '../../assets/images/icons/config_profile.svg'
+import AddProductIcon from '../../assets/images/icons/plus.svg'
+import Trash from '../../assets/images/icons/trash.svg'
+import Shutdown from '../../assets/images/icons/shutdown.svg'
+import { Link } from 'react-router-dom'
+import ProductInterface from '../../interfaces/ProductInterface'
+import { FaEye, FaPencilAlt, FaTrash } from 'react-icons/fa'
+
+const SalesmanHub = () => {
+  const [products, setProducts] = useState<ProductInterface[]>([])
+
+  const getDataByApi = async () => {
+    const response_salesman = await api.get<ProductInterface[]>('/salesman/show', {
+      headers: {
+        searchid: localStorage.getItem('salesmanid') }
+    })
+
+    console.log(response_salesman.data)
+
+    const response_products = await api.get('/products/show/salesman', {
+      headers: {
+        salesman_id: localStorage.getItem('salesmanid')
+      }
+    })
+
+    setProducts(response_products.data)
+  }
+
+  useEffect(() => {
+    getDataByApi()
+  })
+  return (
+    <Container>
+      <h1>Seja bem vindo vendedor</h1>
+      <h2>O que deseja fazer ?</h2>
+      <section>
+        <Link className='link' to='/'>
+          <figure>
+            <img src={EditProfileIcon} alt='profile config icon' />
+            <figcaption>Editar perfil</figcaption>
+          </figure>
+        </Link>
+        <Link className='link' to='/salesman/create/product'>
+          <figure>
+            <img src={AddProductIcon} alt='add product' />
+            <figcaption>Adicionar produto</figcaption>
+          </figure>
+        </Link>
+        <Link className='link' to='/salesman/delete/product'>
+          <figure>
+            <img src={Trash} alt='remove product'/>
+            <figcaption>Remover produto(s)</figcaption>
+          </figure>
+        </Link>
+
+        <Link className='link' to='/'>
+          <figure>
+            <img src={Shutdown} alt='Shutdown button'/>
+            <figcaption>Desligar</figcaption>
+          </figure>
+        </Link>
+      </section>
+      <h1 className='product-title'>Este são os produtos adicionados por você: </h1>
+      <article>
+        <ul>
+          {products.map(product => {
+            return (
+              <li key={product.product_image}>
+                <figure>
+                  <img src={`http://localhost:3333/uploads/${product.product_image}`} />
+                  <figcaption>
+                    <strong>Nome: </strong>{product.product_name}<br/>
+                    <strong>Marca: </strong>{product.brand}<br/>
+                    <strong>Tipo: </strong>{product.kind_prod}<br/>
+                    <strong>Quantidade: </strong>{product.amount}<br/>
+                  </figcaption>
+                </figure>
+                <span>
+                  <FaEye color='#0f0' size={36} />
+                  <FaPencilAlt color='#FBE743' size={36} />
+                  <FaTrash color='#f00' size={36} />
+                </span>
+              </li>
+            )
+          })}
+        </ul>
+      </article>
+    </Container>
+  )
+}
+
+export default SalesmanHub
---
+++ web/src/views/SalesmanHub/styled.ts
@@ -0,0 +1,103 @@
+import styled from 'styled-components'
+
+export const Container = styled.main`
+  display: flex;
+  flex-direction: column;
+  padding: 30px;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+
+  h2 {
+    margin: 30px;
+    color: #0a0a0a;
+  }
+
+  section {
+    display: flex;
+    flex-direction: row;
+  }
+
+  section > .link > figure{
+    margin: 15px;
+    border: solid 3px #acacac;
+    padding: 30px;
+    display: flex;
+    flex-direction:column;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+    cursor: pointer;
+    color: #0a0a0a;
+  }
+
+  section > .link {
+    text-decoration: none;
+  }
+
+  section > .link > figure > img {
+    width: 80px;
+    margin: 5px;
+  }
+
+  .product-title{
+    margin-top: 50px;
+  }
+
+  article {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+
+  article > ul{
+    list-style-type: none;
+    padding: 15px;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+
+  article > ul > li {
+    display:flex;
+    flex-direction: row;
+    border: solid 3px #acacac;
+    border-radius: 10px;
+    margin: 10px;
+    justify-content: space-between;
+    width: 70%;
+    padding: 15px;
+  }
+
+  article > ul > li > figure{
+    display:flex;
+    flex-direction: row;
+    align-items: center;
+    text-align: left !important;
+  }
+
+  article > ul > li > figure > img {
+    width: 300px;
+  }
+
+  article > ul > li > figure > figcaption {
+    font-size: 18pt;
+  }
+
+  article > ul > li > span{
+    display: flex;
+    flex-direction: row;
+    align-items: flex-end;
+    justify-content: center;
+  }
+
+  article > ul > li > span > *{
+    margin: 15px;
+    cursor: pointer;
+  }
+
+`
