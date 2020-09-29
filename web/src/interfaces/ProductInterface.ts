56d54d463d9e1e8b7efc9e55f37be6d653df628d
Gustavo S. Melo <gsantos15569@gmail.com>
2020-09-29 17:59:58 -0300

    add interface of product

---
+++ web/src/interfaces/ProductInterface.ts
@@ -0,0 +1,9 @@
+export default interface Product {
+  product_name: string,
+  product_image: string,
+  amount: number,
+  brand: string,
+  description: string,
+  kind_prod: string,
+  product_price: number
+}
