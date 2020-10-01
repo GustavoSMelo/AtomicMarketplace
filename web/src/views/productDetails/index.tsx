18e963a52c75d5d9d8b8f379859b534312432fd9
Gustavo S. Melo <gsantos15569@gmail.com>
2020-10-01 00:02:13 -0300

    page of details product done, only need to repair a system of favorite product

--- web/src/views/productDetails/index.tsx
+++ web/src/views/productDetails/index.tsx
@@ -1,29 +1,127 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import './style.css'
 import Header from '../../components/navbar'
-import { FaShoppingBag, FaRegHeart, FaStar } from 'react-icons/fa'
+import { FaShoppingBag, FaRegHeart, FaStar, FaHeart } from 'react-icons/fa'
+import { RouteComponentProps } from 'react-router-dom'
+import api from '../../api'
+import ProductInterface from '../../interfaces/ProductInterface'
+import CommentaryInterface from '../../interfaces/CommentaryInterface'
+import UserInterface from '../../interfaces/UserInterface'
+import UserProfileIcon from '../../assets/images/profile.svg'
+import FavoriteInterface from '../../interfaces/FavoriteInterface'
+import PopupCard from '../../components/popupStatusCard'
 
-const Details = () => {
+const Details:React.FC<RouteComponentProps> = ({ match }) => {
+  const [product_name, setProduct_name] = useState('')
+  const [description, setDescription] = useState('')
+  const [amount, setAmount] = useState(0)
+  const [product_image, setProduct_image] = useState('')
+  const [product_price, setProduct_price] = useState(0)
+  const [product_id, setProduct_id] = useState(0)
+  const [Commentary, setCommentary] = useState<CommentaryInterface []>([])
+  const [User, setUser] = useState<UserInterface[]>([])
+  const [favorites, setFavorites] = useState<FavoriteInterface[]>([])
+  const [status, setStatus] = useState(<></>)
+
+  const getDataByAPI = async () => {
+    const { id } = match.params as any
+    try{
+      const response = await api.get<ProductInterface>('/products/show/only', { headers: {
+        id
+      }})
+
+      setProduct_name(response.data.product_name)
+      setDescription(response.data.description)
+      setAmount(response.data.amount)
+      setProduct_image(response.data.product_image)
+      setProduct_price(response.data.product_price)
+
+      setProduct_id(id)
+
+      const commentary_response = await api.get<CommentaryInterface[]>('/commentary', {
+        headers: {
+          productid: id
+        }
+      })
+
+      setCommentary(commentary_response.data)
+
+      const user_response = await api.get<UserInterface[]>('/user')
+
+      setUser(user_response.data)
+
+      const favorite_response = await api.get<FavoriteInterface[]>('/favorites', {
+        headers: {
+          authorization: `Bearer ${localStorage.getItem('token')}`,
+          userid: localStorage.getItem('iduser')
+        }
+      })
+
+      setFavorites(favorite_response.data)
+    } catch (err) {
+      console.log({ Error: err })
+    }
+  }
+
+  const insertFavorite = async (productid : Number) => {
+    try {
+      await api.post('/favorites', {}, {
+        headers: {
+          authorization: `Bearer ${localStorage.getItem('token')}`,
+          userid: localStorage.getItem('iduser'),
+          productid
+        }
+      })
+    } catch (err) {
+      setStatus(<PopupCard backgroundcolor='#FA6450' textcolor='#5C241D' content='Erro ao favoritar produto '/>)
+      console.log({Error: err})
+    }
+  }
+
+  const deleteFavorite = async (favoriteid: Number) => {
+    try {
+      await api.delete('/favorites', {
+        headers: {
+          favoriteid,
+          authorization: `Bearer ${localStorage.getItem('token')}`
+        }
+      })
+    } catch (err) {
+      setStatus(<PopupCard backgroundcolor='#FA6450' textcolor='#5C241D' content='Erro ao remover favorito do produto'/>)
+    }
+  }
+
+  useEffect(() => {
+    getDataByAPI()
+  }, [])
+
   return (
     <>
       <Header />
       <main className='container' data-testid='product'>
         <article className='product'>
           <figure data-testid='photo' className='photo'>
-            <img src='https://cdn.pixabay.com/photo/2013/07/12/18/20/chucks-153310_960_720.png' alt='photo_product' />
+            <img src={`http://localhost:3333/uploads/${product_image}`} alt='photo_product' />
           </figure>
           <aside className='informations'>
             <section className='title' data-testid='name'>
-              <h1>All-star</h1>
-              <span className='heart' data-testid='favorite'>
-                <FaRegHeart color={'#f00'} />
-              </span>
+              <h1>{product_name}</h1>
+              {favorites.find(fav => fav.id_user === Number(localStorage.getItem('iduser')) &&
+                fav.id_product === product_id) ?
+                <span className='heart'>
+                  <FaHeart color={'#f00'} onClick={() => deleteFavorite(favorites.find(fav => fav.id_user === Number(localStorage.getItem('iduser')) &&
+                fav.id_product === product_id).id)} />
+                </span> :
+                <span className='heart' onClick={() => insertFavorite(product_id)}>
+                  <FaRegHeart color={'#f00'} />
+                </span>
+              }
             </section>
             <p data-testid='description'>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur fuga eaque delectus amet harum similique unde excepturi architecto nihil officia, ea voluptatibus illo necessitatibus iste enim! Nihil ipsa tenetur non.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis, impedit exercitationem, fugiat aliquid laudantium iure expedita eius rem ratione neque non nemo, deserunt repellendus? Facere quidem nisi a error molestias.
+              {description}
             </p>
             <section className='RowContainer'>
-              <h1 data-testid='price'>BRL 120.00</h1> <h2 data-testid='amount'>15 restantes</h2>
+              <h1 data-testid='price'>BRL {product_price}</h1> <h2 data-testid='amount'>{amount} restantes</h2>
             </section>
             <button type='button' className='cart' data-testid='cart'>
               <FaShoppingBag /> Adiciona na sacola
@@ -32,23 +130,31 @@ const Details = () => {
         </article>
         <h1 data-testid='rating-product'>Reviews:
           <br />
-          4.3 <FaStar color={'#C7BD61'} />
+          {Commentary.length <= 0 ? 0 : Commentary.filter(comment => comment.id_product !== product_id).map(comment => {
+            let sum = 0
+            sum += comment.rating
+            sum /= Commentary.length
+
+            return sum
+          })}<FaStar color={'#C7BD61'} />
         </h1>
         <ul className='ReviewContainer' data-testid='commentaries'>
-          <li className='Review'>
-            <section className='RowContainer'>
-              <figure>
-                <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png" alt='avatar' />
-              </figure>
-              <h1 data-testid='nameUser'>Name of person</h1>
-            </section>
-            <p className='commentary' data-testid='commentary'>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti natus atque exercitationem, dolore earum aliquam mollitia facilis! Nesciunt, blanditiis ducimus animi veritatis labore, quam impedit consequatur magnam nulla enim cupiditate. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis provident pariatur perspiciatis, temporibus sequi error molestias minima aliquid ratione, nisi dolorum dignissimos beatae nemo molestiae quidem consequatur dicta ad voluptate?
-            </p>
-            <small data-testid='rating-commentary'> 3.0 <FaStar color={'#C7BD61'} /></small>
-          </li>
+          {Commentary.map(comment => (
+            <li className='Review' key={comment.id}>
+              <section className='RowContainer'>
+                <figure>
+                  <img src={UserProfileIcon } alt='avatar' />
+                </figure>
+                <h1 data-testid='nameUser'>{User.find(usr => usr.id === comment.id_user).name}</h1>
+              </section>
+              <p className='commentary' data-testid='commentary'>
+                {comment.commentary}
+              </p>
+              <small data-testid='rating-commentary'> {comment.rating} <FaStar color={'#C7BD61'} /></small>
+            </li>))}
         </ul>
       </main>
+      {status}
     </>
   )
 }
