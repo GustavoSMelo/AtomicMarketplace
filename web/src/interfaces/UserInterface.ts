2fbdc043bed89f410496150c40cf039014a61a50
Gustavo S. Melo <gsantos15569@gmail.com>
2020-09-30 23:53:27 -0300

    Interface of user

---
+++ web/src/interfaces/UserInterface.ts
@@ -0,0 +1,13 @@
+interface UserInterface {
+  id: number,
+  email: string,
+  address: string,
+  address_number: string,
+  cep: string,
+  cellphone: string,
+  state: string,
+  neighborhood: string,
+  name: string
+}
+
+export default UserInterface
