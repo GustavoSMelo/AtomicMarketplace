d2d3050b8dce4361cdc19a27a8b8ea6225dee8e0
Gustavo S. Melo <gsantos15569@gmail.com>
2020-09-30 23:59:29 -0300

    logic of delete product done

--- web/src/views/DeleteProduct/index.tsx
+++ web/src/views/DeleteProduct/index.tsx
@@ -1,27 +1,70 @@
-import React from 'react';
-import Navbar from '../../components/navbar';
+import React, { useState, useEffect } from 'react'
+import Navbar from '../../components/navbar'
 import { RiShoppingCartLine, RiDeleteBinLine } from 'react-icons/ri'
-import { Container } from './styled';
-import ListProduct from '../../components/ListProduct';
+import { Container } from './styled'
+import ListProduct from '../../components/ListProduct'
+import ProductInterface from '../../interfaces/ProductInterface'
+import api from '../../api'
+import PopupCard from '../../components/popupStatusCard'
 
 const DeleteProduct: React.FC = () => {
+  const [products, setProducts] = useState<ProductInterface[]>([])
+  const [status, setStatus] = useState(<></>)
+
+  const deleteProductMethod = async id => {
+    try {
+      const response = await api.delete('/products', { headers: {
+        searchid: id,
+        authorization: `Bearer ${localStorage.getItem('token')}`
+      }})
+
+      if (response.status === 200) {
+        products.filter(product => product.id === id)
+      }
+
+      setStatus(<PopupCard backgroundcolor='#51B556' textcolor='#295C2C' content='product deleted with success' />)
+    } catch (err) {
+      setStatus(<PopupCard backgroundcolor='#FA6450' textcolor='#5C241D' content='Error to delete product, try again ' />)
+    }
+  }
+
+  const getDataByAPI = async () => {
+    const response = await api.get<ProductInterface[]>('/products', {
+      headers: {
+        salesman_id: localStorage.getItem('salesmanid')
+      }
+    })
+
+    setProducts(response.data)
+  }
+
+  useEffect(() => {
+    setTimeout(() => {
+      setStatus(<></>)
+    }, 3000)
+  }, [status])
+
+  useEffect(() => {
+    getDataByAPI()
+  }, [products])
+
   return (
     <>
       <Navbar />
       <Container>
         <h2>Quais Produtos deseja deletar?</h2>
         <h4> <RiShoppingCartLine size={18} /> Histórico de produtos </h4>
-        <ListProduct image="a" nameProduct="nike air jordan" date="14/10/2020" quantity={25} />
-        <ListProduct image="b" nameProduct="nike 99" date="15/19/2020" quantity={105} />
-
-        <ListProduct image="c" nameProduct="Adidas bluez" date="14/10/2021" quantity={75} />
-
-        <ListProduct image="d" nameProduct="nick x travis" date="14/01/2020" quantity={50} />
-
-
+        {products.map(product => <ListProduct
+          key={product.id}
+          image={`http://localhost:3333/uploads/${product.product_image}`}
+          nameProduct={product.product_name}
+          date={String(product.created_at)}
+          quantity={product.amount}
+          onClickFunction={() => deleteProductMethod(product.id)}/>)}
       </Container>
+      {status}
     </>
-  );
+  )
 }
 
-export default DeleteProduct;
+export default DeleteProduct
