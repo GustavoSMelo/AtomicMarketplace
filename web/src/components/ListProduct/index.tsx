c40dfc85b0dfc0ce7e81429eb50610f6832793ac
Gustavo S. Melo <gsantos15569@gmail.com>
2020-09-30 23:58:30 -0300

    update component of view delete product

--- web/src/components/ListProduct/index.tsx
+++ web/src/components/ListProduct/index.tsx
@@ -1,42 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react'
 import { RiDeleteBinLine } from 'react-icons/ri'
 import { RiEditFill } from 'react-icons/ri'
-import { Link } from 'react-router-dom';
-
-import { Main } from './styled';
+import { Link } from 'react-router-dom'
+import { Main } from './styled'
+import api from '../../api'
+import PopupCard from '../popupStatusCard'
 
 interface listProductProps {
   image: string;
   nameProduct: string;
   date: string;
   quantity: number;
+  onClickFunction?: Function;
 }
-const ListProduct: React.FC<listProductProps> = ({ image, nameProduct, date, quantity }) => {
+
+const ListProduct: React.FC<listProductProps> = ({ image, nameProduct, date, quantity, onClickFunction }) => {
+  const [status, setStatus] = useState(<></>)
+
+  useEffect(() => {
+    setTimeout(() => {
+      setStatus(<></>)
+    }, 3000)
+  }, [status])
+
   return <>
     <Main>
       <section>
-
         <img src={image} alt="imagem do produto" />
-
         <div className="data-product">
           <p>Nome do Produto: <strong>{nameProduct}</strong></p>
           <p className="date-product">Adicionado em: <strong>{date}</strong></p>
           <p>Quantidade: <strong>{quantity}</strong></p>
         </div>
-
-
         <div className="buttons-delete">
-          <button><Link to="/salesman/edit/product"> Editar Produto <RiEditFill /></Link></button>
-          <button>Deletar Todos <RiDeleteBinLine color={"red"} /></button>
-          <button>Deletar um <RiDeleteBinLine color={"red"} /></button>
+          <button onClick={() => onClickFunction()}>Deletar <RiDeleteBinLine color='#f00' /></button>
         </div>
-
-
       </section>
-
     </Main>
-
-  </>;
+    {status}
+  </>
 }
 
-export default ListProduct;
+export default ListProduct
--- web/src/components/ListProduct/styled.ts
+++ web/src/components/ListProduct/styled.ts
@@ -1,65 +1,66 @@
 import styled from 'styled-components'
 
 export const Main = styled.main`
-section{
-  display:flex;
-  justify-content:space-around;
-  margin-bottom:10px;
-  padding:10px;
-}
-section .image-product{
-  background-color: #ccc;
-padding: 5% 3% 5%;
-  border-radius:5px;
-  margin-right:2rem;
-}
-section .data-product{
-  display:flex;
-  width:600px;
-max-width:600px;
-  justify-content:space-between;
-}
-section .data-product>p{
-  margin-right:15px;
-  white-space:normal;
-}
-section .buttons-delete>button{
-  background-color: #fff;
-  border: 1px solid #000;
-  padding:15px;
-  font-size: 14px;
-  margin:5px 3px 5px;
-  height:63px;
-  width:100px;
-  max-width: 100px;
-  max-height:63px;
-}
-.buttons-delete{
-  width:320px;
- max-width:320px;
-}
-section .buttons-delete>button:hover{
-  background-color: #000;
-  color:#ccc;
-  cursor: pointer;
-}
-section .data-product>p{
-  white-space:nowrap;
-}
-a{
-  text-decoration:none;
-  color: #000;
-}
-@media(max-width:1150px){
-  .date-product{
-    display:none;
+  section{
+    display:flex;
+    justify-content:space-around;
+    margin-bottom:10px;
+    padding:10px;
+  }
+
+  section > img{
+    width: 180px;
+  }
+
+  section .image-product{
+    background-color: #ccc;
+  padding: 5% 3% 5%;
+    border-radius:5px;
+    margin-right:2rem;
+  }
+  section .data-product{
+    display:flex;
+    width:600px;
+  max-width:600px;
+    justify-content:space-between;
+  }
+  section .data-product>p{
+    margin-right:15px;
+    white-space:normal;
+  }
+  section .buttons-delete>button{
+    background-color: #fff;
+    border: 1px solid #000;
+    padding:15px;
+    font-size: 14px;
+    margin:5px 3px 5px;
+    height:63px;
+    width:100px;
+    max-width: 100px;
+    max-height:63px;
   }
   .buttons-delete{
-    max-width:110px;
+    width:320px;
+  max-width:320px;
   }
-}
-
-
-
-
+  section .buttons-delete>button:hover{
+    background-color: #000;
+    color:#ccc;
+    cursor: pointer;
+  }
+  section .data-product>p{
+    white-space:nowrap;
+  }
+  a{
+    text-decoration:none;
+    color: #000;
+  }
+  @media(max-width:1150px){
+    .date-product{
+      display:none;
+    }
+    .buttons-delete{
+      max-width:110px;
+    }
+  }
 `
