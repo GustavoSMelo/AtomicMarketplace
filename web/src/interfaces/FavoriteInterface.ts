f0badda6889e5b3f3321d078db631aaca7138166
Gustavo S. Melo <gsantos15569@gmail.com>
2020-10-02 01:24:09 -0300

    add new parameter in interface

--- web/src/interfaces/FavoriteInterface.ts
+++ web/src/interfaces/FavoriteInterface.ts
@@ -1,7 +1,7 @@
 interface FavoriteInterface {
-  id: number,
-  id_user: number,
-  id_product: number
+  id: number;
+  id_user: number;
+  id_product: number;
 }
 
 export default FavoriteInterface
