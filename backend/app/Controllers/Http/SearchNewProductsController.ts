85152ffada40373d93cd1cedc0d6618eb8714d0d
Gustavo S. Melo <gsantos15569@gmail.com>
2020-10-05 00:39:42 -0300

    Logic to seach new products in database

---
+++ backend/app/Controllers/Http/SearchNewProductsController.ts
@@ -0,0 +1,16 @@
+// import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import ProductModel from '../../Models/Product'
+
+export default class SearchNewProductsController {
+  public async Index () {
+    let qntd = 0
+    const product = await ProductModel.query().select('*').orderBy('created_at')
+    const filtredProduct = product.map(item => {
+      if(qntd <= 15) {
+        return item
+      }
+    })
+
+    return filtredProduct
+  }
+}
