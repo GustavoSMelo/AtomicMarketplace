0483e646d27280f1049a94f4a2775dc5c0b42766
Gustavo S. Melo <gsantos15569@gmail.com>
2020-10-06 01:53:37 -0300

    add commentaries logic about product

--- web/src/views/productDetails/index.tsx
+++ web/src/views/productDetails/index.tsx
@@ -1,8 +1,8 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import './style.css'
 import Header from '../../components/navbar'
 import { FaShoppingBag, FaRegHeart, FaStar, FaHeart } from 'react-icons/fa'
-import { RouteComponentProps } from 'react-router-dom'
+import { RouteComponentProps, useRouteMatch } from 'react-router-dom'
 import api from '../../api'
 import ProductInterface from '../../interfaces/ProductInterface'
 import CommentaryInterface from '../../interfaces/CommentaryInterface'
@@ -10,6 +10,9 @@ import UserInterface from '../../interfaces/UserInterface'
 import UserProfileIcon from '../../assets/images/profile.svg'
 import FavoriteInterface from '../../interfaces/FavoriteInterface'
 import PopupCard from '../../components/popupStatusCard'
+import VendasInterface from '../../interfaces/VendasInterface'
+import Input from '../../components/input'
+import { useHistory } from 'react-router-dom'
 
 const Details:React.FC<RouteComponentProps> = ({ match }) => {
   const [product_name, setProduct_name] = useState('')
@@ -22,6 +25,10 @@ const Details:React.FC<RouteComponentProps> = ({ match }) => {
   const [User, setUser] = useState<UserInterface[]>([])
   const [status, setStatus] = useState(<></>)
   const [favorites, setFavorites] = useState<FavoriteInterface []>([])
+  const [venda, setVenda] = useState<VendasInterface []>([])
+  const [review, setReview] = useState('')
+  const [rating, setRating] = useState(0)
+  const history = useHistory()
 
   const getDataByAPI = async () => {
     const { id } = match.params as any
@@ -36,7 +43,7 @@ const Details:React.FC<RouteComponentProps> = ({ match }) => {
       setProduct_image(response.data.product_image)
       setProduct_price(response.data.product_price)
 
-      setProduct_id(id)
+      setProduct_id(Number(id))
 
       const commentary_response = await api.get<CommentaryInterface[]>('/commentary', {
         headers: {
@@ -56,6 +63,15 @@ const Details:React.FC<RouteComponentProps> = ({ match }) => {
         }
       })
       setFavorites(favorite_response.data)
+
+      const venda_response = await api.get<VendasInterface[]>('/venda', {
+        headers: {
+          authorization: `Bearer ${localStorage.getItem('token')}`,
+          userid: Number(localStorage.getItem('iduser'))
+        }
+      })
+
+      setVenda(venda_response.data)
     } catch (err) {
       console.log({ Error: err })
     }
@@ -72,7 +88,7 @@ const Details:React.FC<RouteComponentProps> = ({ match }) => {
       })
     } catch (err) {
       setStatus(<PopupCard backgroundcolor='#FA6450' textcolor='#5C241D' content='Erro ao favoritar produto '/>)
-      console.log({Error: err})
+      console.log({ Error: err })
     }
   }
 
@@ -89,9 +105,51 @@ const Details:React.FC<RouteComponentProps> = ({ match }) => {
     }
   }
 
+  const addProductInCart = (productid : Number) => {
+    const cart = localStorage.getItem('cart')
+    const arryCart = cart ? cart.split(',') : []
+
+    if (arryCart.find(index => Number(index) === productid)) {
+      return setStatus(<PopupCard backgroundcolor='#FA6450' textcolor='#5C241D' content='Produto já inserido dentro do carrinho '/>)
+    } else {
+      arryCart.push(productid.toString())
+      localStorage.setItem('cart', arryCart.toString())
+
+      return setStatus(<PopupCard backgroundcolor='#51B556' textcolor='#295C2C' content='produto inserido no carrinho com sucesso '/>)
+    }
+  }
+
+  const DoComment = async () => {
+    try {
+      if (rating <= 0 || rating > 5) {
+        return setStatus(<PopupCard backgroundcolor='#FA6450' textcolor='#5C241D' content='Informe uma nota entre 1 e 5'/>)
+      } else if (review === '') {
+        return setStatus(<PopupCard backgroundcolor='#FA6450' textcolor='#5C241D' content='Insira seu comentario por favor '/>)
+      } else {
+        await api.post('/commentary', { commentary: review, rating }, {
+          headers: {
+            authorization: `Bearer ${localStorage.getItem('token')}`,
+            userid: Number(localStorage.getItem('iduser')),
+            productid: product_id
+          }
+        })
+
+        history.push(`/product/details/${product_id}`)
+      }
+    } catch (err) {
+      return setStatus(<PopupCard backgroundcolor='#FA6450' textcolor='#5C241D' content='Ocorreu um erro ao realizar o comentario '/>)
+    }
+  }
+
   useEffect(() => {
     getDataByAPI()
   }, [favorites])
+
+  useEffect(() => {
+    setTimeout(() => {
+      setStatus(<></>)
+    }, 5000)
+  }, [status])
 
   return (
     <>
@@ -123,7 +181,7 @@ const Details:React.FC<RouteComponentProps> = ({ match }) => {
             <section className='RowContainer'>
               <h1 data-testid='price'>BRL {product_price}</h1> <h2 data-testid='amount'>{amount} restantes</h2>
             </section>
-            <button type='button' className='cart' data-testid='cart'>
+            <button onClick={() => addProductInCart(product_id)} type='button' className='cart' data-testid='cart'>
               <FaShoppingBag /> Adiciona na sacola
             </button>
           </aside>
@@ -138,14 +196,27 @@ const Details:React.FC<RouteComponentProps> = ({ match }) => {
             return sum
           })}<FaStar color={'#C7BD61'} />
         </h1>
+        {venda.find(sell => sell.id_user === Number(localStorage.getItem('iduser')) && sell.id_product === product_id) ? (
+          <form>
+            <h2>Deixe o seu review para possiveis outros compradores se interessarem tambem </h2>
+            <textarea className='review-comment' value={review} onChange={e => setReview(e.target.value)}></textarea>
+            <Input placeholder='Nota para o produto' onTextChanged={setRating} value={rating} type='number' min='0' max='5' />
+            <button type='button' className='btnReview' onClick={() => DoComment()}>Avaliar</button>
+          </form>
+        )
+          : <></>}
         <ul className='ReviewContainer' data-testid='commentaries'>
           {Commentary.map(comment => (
             <li className='Review' key={comment.id}>
               <section className='RowContainer'>
                 <figure>
-                  <img src={UserProfileIcon } alt='avatar' />
+                  <img src={UserProfileIcon} alt='avatar' />
                 </figure>
-                <h1 data-testid='nameUser'>{User.find(usr => usr.id === comment.id_user).name}</h1>
+                <h1 data-testid='nameUser'>{User.map(usr => {
+                  if (usr.id === comment.id_user) {
+                    return usr.name
+                  }
+                })}</h1>
               </section>
               <p className='commentary' data-testid='commentary'>
                 {comment.commentary}
