a4b138274744fb10ad338933650824454dd5baa0
Gustavo S. Melo <gsantos15569@gmail.com>
2020-10-14 01:58:11 -0300

    logic of edit user done

--- web/src/views/EditUser/index.tsx
+++ web/src/views/EditUser/index.tsx
@@ -3,6 +3,10 @@ import { Container } from './styled'
 import api from '../../api'
 import UserInterface from '../../interfaces/UserInterface'
 import Input from '../../components/input'
+import Navbar from '../../components/navbar'
+import { FaArrowLeft } from 'react-icons/fa'
+import { useHistory } from 'react-router-dom'
+import PopupCard from '../../components/popupStatusCard'
 
 const EditUser:React.FC = () => {
   const [name, setName] = useState('')
@@ -14,13 +18,11 @@ const EditUser:React.FC = () => {
   const [state, setState] = useState('')
   const [neighborhood, setNeighborhood] = useState('')
   const [password, setPassword] = useState('')
+  const [status, setStatus] = useState(<></>)
+  const history = useHistory()
 
   const getDataByAPI = async () => {
-    const response = await api.post<UserInterface>('/user/show', {}, {
-      headers: {
-        searchid: localStorage.getItem('iduser')
-      }
-    })
+    const response = await api.post<UserInterface>('/user/show', { searchid: localStorage.getItem('iduser') })
 
     setName(response.data.name)
     setEmail(response.data.email)
@@ -32,21 +34,76 @@ const EditUser:React.FC = () => {
     setNeighborhood(response.data.neighborhood)
   }
 
+  const handleEditUser = async () => {
+    try {
+      await api.put('/user', { name, email, address, address_number, cep, cellphone, state, neighborhood, password }, {
+        headers: {
+          authorization: `Bearer ${localStorage.getItem('token')}`,
+          searchid: Number(localStorage.getItem('iduser'))
+        }
+      })
+
+      history.push('/user/profile')
+    } catch (err) {
+      setStatus(<PopupCard backgroundcolor='#FA6450' textcolor='#5C241D' content='Erro ao favoritar produto '/>)
+      console.log({ Error: err })
+    }
+  }
+
   useEffect(() => {
     getDataByAPI()
   }, [])
+
+  useEffect(() => {
+    setTimeout(() => {
+      setStatus(<></>)
+    }, 3500)
+  }, [status])
+
   return (
-    <Container>
-      <form>
-        <Input placeholder='insert your name here' type='text' value={name} onTextChanged={setName}/>
-        <Input placeholder='insert your address here' type='text' value={address} onTextChanged={setAddress}/>
-        <Input placeholder='insert your address number here' type='text' value={address_number} onTextChanged={setAddress_number}/>
-        <Input placeholder='insert your cep here' type='text' value={cep} onTextChanged={setCep}/>
-        <Input placeholder='insert your cellphone here' type='text' value={cellphone} onTextChanged={setCellphone}/>
-        <Input placeholder='insert your state here' type='text' value={state} onTextChanged={setState}/>
-        <Input placeholder='insert your neighborhood here' type='text' value={neighborhood} onTextChanged={setNeighborhood}/>
-      </form>
-    </Container>
+    <>
+      <Navbar />
+      <Container>
+        <form>
+          <h1>Editar usuario: </h1>
+          <FaArrowLeft size={30} style={{ margin: 15 }} onClick={() => history.goBack()}/>
+          <span>
+            <label>Nome: </label><Input placeholder='insert your name here' type='text' value={name} onTextChanged={setName}/>
+          </span>
+          <span>
+            <label>Nome: </label><Input placeholder='insert your email here' type='text' value={email} onTextChanged={setEmail}/>
+          </span>
+          <span>
+            <label>Endereço: </label><Input placeholder='insert your address here' type='text' value={address} onTextChanged={setAddress}/>
+          </span>
+          <span>
+            <label>Numero da casa: </label><Input placeholder='insert your address number here' type='text' value={address_number} onTextChanged={setAddress_number}/>
+          </span>
+          <span>
+            <label>CEP: </label><Input placeholder='insert your cep here' type='text' value={cep} onTextChanged={setCep}/>
+          </span>
+          <span>
+            <label>Celular: </label>
+            <Input placeholder='insert your cellphone here' type='text' value={cellphone} onTextChanged={setCellphone}/>
+          </span>
+          <span>
+            <label>Estado: </label>
+            <Input placeholder='insert your state here' type='text' value={state} onTextChanged={setState}/>
+          </span>
+          <span>
+            <label>Bairro: </label>
+            <Input placeholder='insert your neighborhood here' type='text' value={neighborhood} onTextChanged={setNeighborhood}/>
+          </span>
+
+          <span>
+            <label>Password: </label><Input placeholder='insert your password here' type='password' value={password} onTextChanged={setPassword}/>
+          </span>
+
+          <button type='button' onClick={() => handleEditUser()}>Editar</button>
+        </form>
+      </Container>
+      {status}
+    </>
   )
 }
 
--- web/src/views/EditUser/styled.ts
+++ web/src/views/EditUser/styled.ts
@@ -2,4 +2,44 @@ import styled from 'styled-components'
 
 export const Container = styled.main`
   display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  form {
+    display: flex;
+    flex-direction: column;
+  }
+
+  form > h1{
+    text-align: center;
+    margin-top: 120px;
+    margin-bottom: 30px;
+  }
+
+  form > span {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+  form > button {
+    padding: 30px;
+    font-size: 16pt;
+    margin-top: 30px;
+    margin-bottom: 30px;
+    cursor: pointer;
+    background-color: #000;
+    color: #fff;
+    font-weight: bold;
+    border: none;
+    border-radius: 5px;
+    transition: 0.35s;
+  }
+
+  form > button:hover{
+    background-color: #202020;
+    transition: 0.35s;
+  }
 `
